053d796a92c6eb1630b7b891f4e0a3d535241518
Jerzy <01199216@pw.edu.pl>
2026-05-27 17:55:21 +0200

    minor: pointless debug-print

--- R/chull.R
+++ R/chull.R
@@ -2,7 +2,6 @@ algorithms <- c("graham", "andrew", "quick")
 
 C.chull <- function(x, y, algorithm = "andrew") {
     alg <- match(algorithm, algorithms)
-    print(alg)
     if(is.na(alg)) stop("Erroneous algorithm")
     .Call("krak", as.numeric(x), as.numeric(y), as.integer(alg-1), 1L)
 }
